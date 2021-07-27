4995ade0778bdbb6654d21ea564072b0ec749655
Karoline Toledo <82004773+karooltooledo@users.noreply.github.com>
2021-07-27 13:44:30 -0300

    Acrescentei os métodos Get, Post, Put e Delete.

--- controller/PostagemController.java
+++ controller/PostagemController.java
@@ -25,4 +25,29 @@ public class PostagemController {
 		return ResponseEntity.ok(repository.findAll());
 	}
 	
+ 	@GetMapping("/{id}")	
+	public ResponseEntity<Postagem> getByIdIfElse(@PathVariable long id) {
+		return postagemrepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	 //Busca o titulo na Postagem
+	@GetMapping("/titulo/{titulo}")	
+ 	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemrepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	//Criar Postagem
+	@PostMapping
+ 	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemrepository.save(postagem));
+	}
+	//Alterar Postagem
+	@PutMapping	
+ 	public ResponseEntity<Postagem> putPostagem(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(postagemrepository.save(postagem));
+	}
+	//Deletar Postagem
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable long id) {
+		postagemrepository.deleteById(id);
+	}
 }
